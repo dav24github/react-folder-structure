47fecb9eb20155a377ec4c3535baa03ee81295ef
dav24github <davidthbsossa@gmail.com>
2025-06-20 15:39:52 -0400

    update

---
+++ src/app/pages/home/Home.tsx
@@ -0,0 +1,71 @@
+const Home = () => {
+  const dispatch = useDispatch();
+  const { callEndpoint } = useCallEndpoint();
+  const { settings, getAndStoreOtherSetting } = useSettings();
+  const { user } = useUser();
+  const { person, storeFormsStatus } = usePerson();
+  const { loadedData, error, fetchData } = useFetchSimultaneously([
+    getAndStoreOtherSetting,
+    storeFormsStatus,
+  ]);
+  const [searchParams] = useSearchParams();
+  const fromQuery = searchParams.get("from");
+  const [showPasswordModal, setShowPasswordModal] = useState<boolean>(false);
+  const [showProfileModal, setShowProfileModal] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (user.resetCredencial) setShowPasswordModal(true);
+  }, [user.resetCredencial]);
+
+  const completedForms = Boolean(
+    person.generalInfo.status &&
+      person.education.status &&
+      person.courses.status &&
+      person.experiences.status &&
+      person.emergency.status &&
+      person.relatives.status &&
+      person.hobbies.status
+  );
+
+  const visibleHomeSummary =
+    !settings.otherSettings?.generalInfo ||
+    (completedForms && fromQuery != "personal-info");
+
+  return (
+    <>
+      {showPasswordModal && (
+        <NewPasswordModal
+          onClose={() => setShowPasswordModal(false)}
+        />
+      )}
+      {showProfileModal && (
+        <UploadImageModal
+          size="sm"
+          src={null} // No profile
+          rounded
+          onLater={() => setShowProfileModal(false)}
+        />
+      )}
+      <ErrorBoundary
+        fallbackComponent={<ErrorView handleClick={fetchData} />}
+        loadedData={loadedData}
+        error={error}
+        isPage
+      >
+        <PageLayout paddingRB>
+          <Grid container className={classes.container}>
+            <Banner />
+
+            {!visibleHomeSummary ? (
+              <PersonalInfoButtons completedForms={completedForms} />
+            ) : (
+              <HomeSummary />
+            )}
+          </Grid>
+        </PageLayout>
+      </ErrorBoundary>
+    </>
+  );
+};
+
+export default Home;
